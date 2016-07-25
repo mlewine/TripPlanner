8f2d6073412c02c0f04ee779bb709435c849b6f6
Sidd Meka <Sidd@Siddharthas-MacBook-Air.local>
2016-07-25 16:09:19 +0200

    I changed the data

--- team5-data.sql
+++ team5-data.sql
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-=======
+
 -- teamN-data.sql, where N is your team number, should contain valid SQL code to insert data into your database tables for at least 3 attractions in each of 3 cities, including Metz, Paris, and one or more cities of your choosing. There should be data for each field for each row in each table, including the descriptions of the attractions (the descriptions should be correct, but don't have to be longer than two sentences). In addition, include insert statements to create a trip in Paris on Saturday, 30 July 2016 with at leat 3 activities, at least one of which requires a reservation and at least two of which requires payment (they can be the same activity).
 
->>>>>>> 71ce498129ed61810a3a35a3ace60f811254e86e
 Use team5_schema;
 
 Insert into address values(1,NULL, "Place d'Armes", NULL, 'Metz', 'France', '57000');
@@ -32,19 +30,20 @@ Insert into user values('Harry Potter', 'harrypotter731@hogwarts.edu', NULL, NUL
 Insert into trip values(1,'Metz', '2016-07-07', 'Yes', 'walt.white@notarealemail.com');
 Insert into trip values(2,'Paris', '2016-07-12', 'Yes', 'homer_simpson234@thisisfake.com');
 Insert into trip values(3,'Rome', '2016-05-24', 'Yes', 'harrypotter731@hogwarts.edu');
+Insert into trip values(4,'Paris', '2016-07-30', 'Yes', 'l.knope@notarealemail.com')
 
 
-Insert into attraction values(1,'Metz Cathedral', 'The Metz Cathedral is a Roman Catholic Cathedral located in the heart of Metz, France. The stained glass windows include works by Gothic and Renaissance master glass makers, such as Hermann von Munster and Theobold of Lixheim', 1, 4.50, NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(2,'Centre Pompidou-Metz', 'The Centre Pompidou-Metz is a museum of modern and contemporary arts located in Metz, capital of Lorraine,France. It is a branch of Pompidou arts centre of Paris, and features exhibitions from the French National Museum of Modern Art.', 2, 10.00,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(3,'Museums of Metz', 'It is a labyrinthal organization of rooms, incorporating the ancient Petites Carmes Abbey, the Chèvremont granary, and the Trinitaires church.', 3, 5.00,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(4,'Eiffel Tower', 'The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.', 4, 17.00,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(5,'Louvre', "The Louvre or the Louvre Museum is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the 1st arrondissement.", 5, 15.00,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(6,'Notre Dame', 'Notre Dame is a medeival catholic, and is considered to be one of the best examples of Gothic architecture in the world. Making the climb to the top of the 300 foot towers gives one of the best views in the city.', 6, 8.50,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(7,'58 Tour Eiffel', 'Dine in the heart of Parisian nightlife...When the sun goes down, 58 Tour Eiffel takes on a new dimension. You will be greeted by a hostess who will lead you to your table where the atmosphere of luxury will surround you', 4, 85.00,NULL,NULL,NULL,NULL,1);
-Insert into attraction values(8,'Brunch boat cruise', 'Step on board Paris en Scène and experience our meal prepared on board according to the best bistro traditions by our chef. The only requirement is that you get comfortable in our large leather armchairs, while admiring the view of the Seine', 7, 39.00,NULL,NULL,NULL,NULL,2);
-Insert into attraction values(9,'Colosseum', 'The Colosseum is located just east of the Roman Forum and was built to a practical design, with its 80 arched entrances allowing easy access to 55,000 spectators', 8, 15.50,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(10,'Pantheon', 'It is one of the best-preserved of all Ancient Roman buildings, in large part because it has been in continuous use throughout its history, and since the 7th century, the Pantheon has been used as a church dedicated to "St. Mary and the Martyrs"', 9, NULL,NULL,NULL,NULL,NULL,NULL);
-Insert into attraction values(11,'Trevi Fountain', 'The Trevi Fountain is a fountain in the Trevi district in Rome, Italy, designed by Italian architect Nicola Salvi and completed by Pietro Bracci', 10, NULL,NULL,NULL,NULL,NULL,NULL);
+Insert into attraction values(1,'Metz Cathedral', 'The Metz Cathedral is a Roman Catholic Cathedral located in the heart of Metz, France. The stained glass windows include works by Gothic and Renaissance master glass makers, such as Hermann von Munster and Theobold of Lixheim', 1, 4.50, NULL);
+Insert into attraction values(2,'Centre Pompidou-Metz', 'The Centre Pompidou-Metz is a museum of modern and contemporary arts located in Metz, capital of Lorraine,France. It is a branch of Pompidou arts centre of Paris, and features exhibitions from the French National Museum of Modern Art.', 2, 10.00,NULL);
+Insert into attraction values(3,'Museums of Metz', 'It is a labyrinthal organization of rooms, incorporating the ancient Petites Carmes Abbey, the Chèvremont granary, and the Trinitaires church.', 3, 5.00,NULL);
+Insert into attraction values(4,'Eiffel Tower', 'The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.', 4, 17.00,NULL);
+Insert into attraction values(5,'Louvre', "The Louvre or the Louvre Museum is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the 1st arrondissement.", 5, 15.00,NULL);
+Insert into attraction values(6,'Notre Dame', 'Notre Dame is a medeival catholic, and is considered to be one of the best examples of Gothic architecture in the world. Making the climb to the top of the 300 foot towers gives one of the best views in the city.', 6, 8.50,NULL);
+Insert into attraction values(7,'58 Tour Eiffel', 'Dine in the heart of Parisian nightlife...When the sun goes down, 58 Tour Eiffel takes on a new dimension. You will be greeted by a hostess who will lead you to your table where the atmosphere of luxury will surround you', 4, 85.00,NULL);
+Insert into attraction values(8,'Brunch boat cruise', 'Step on board Paris en Scène and experience our meal prepared on board according to the best bistro traditions by our chef. The only requirement is that you get comfortable in our large leather armchairs, while admiring the view of the Seine', 7, 39.00,NULL,);
+Insert into attraction values(9,'Colosseum', 'The Colosseum is located just east of the Roman Forum and was built to a practical design, with its 80 arched entrances allowing easy access to 55,000 spectators', 8, 15.50,NULL);
+Insert into attraction values(10,'Pantheon', 'It is one of the best-preserved of all Ancient Roman buildings, in large part because it has been in continuous use throughout its history, and since the 7th century, the Pantheon has been used as a church dedicated to "St. Mary and the Martyrs"', 9, NULL,NULL);
+Insert into attraction values(11,'Trevi Fountain', 'The Trevi Fountain is a fountain in the Trevi district in Rome, Italy, designed by Italian architect Nicola Salvi and completed by Pietro Bracci', 10, NULL,NULL);
 
 Insert into review values(1,'Beautiful church!', 'Metz Cathedral', '2016-07-07', 'walt.white@notarealemail.com',1);
 Insert into review values(2,'Incredible art! Wish I could have spent more time there, but I had to catch a train', 'Centre Pompidou-Metz', '2016-06-12', 'walt.white@notarealemail.com',2);
@@ -69,6 +68,14 @@ Insert into activity values(8,8,2,'Eat Brunch on the Seine River', '2016-07-12 8
 Insert into activity values(9,9,NULL,'Visit Colosseum', '2016-05-24 15:30', '2016-05-24 17:00', 3);
 Insert into activity values(10,10,NULL,'Visit Pantheon', '2016-05-24 12:30', '2016-05-24 13:00', 3);
 Insert into activity values(11,11,NULL,'Visit Trevi Fountain', '2016-05-24 17:45', '2016-05-24 19:00', 3);
+Insert into activity values(12,4, NULL, 'Climb Eiffel Tower', '2016-07-30 17:30','2016-07-30 19:00',4);
+Insert into activity values(13,8,1, 'Eat Dinner', '2016-07-30 19:15','2016-07-30 20:45',4);
+Insert into activity values(14,6,NULL, 'Climb the tower', '2016-07-30 13:15','2016-07-30 14:45',4);
+
+Insert into reservation values(1, '18:30','19:30',7,60)
+Insert into reservation values(2, '19:30','20:30',7,60)
+Insert into reservation values(3, '10:30','11:30',8,60)
+
 
 -- Insert into public_transportation values ('Mettis A/B', 1, 'walt.white@notarealemail.com', 1);
 -- Insert into public_transportation values ('Mettis A/B', 2, 'walt.white@notarealemail.com', 1);
