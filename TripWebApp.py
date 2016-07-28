afad8be3f65457b776a3367e68b95f5231b6cdf9
Matthew Lewine <matthew.lewine@gmail.com>
2016-07-28 21:43:48 +0200

    activities yay

--- TripWebApp.py
+++ TripWebApp.py
@@ -205,15 +205,16 @@ def attractions(attraction):
     cursor.execute("select description, price, requires_reservation from attraction where name = '" + attraction + "'")
     rows = cursor.fetchall()
     column_names = [desc[0] for desc in cursor.description]
+    cursor.execute("select activity.name from activity join attraction using (attraction_id) where attraction.name = '" + attraction + "'")
+    activitynames = cursor.fetchall()[0]
     form = AddAttractionForm()
     if form.validate_on_submit():
-        cursor.execute("insert into activity (activity_id, attraction_id, name) values (%s, %s, %s)")
+        cursor.execute("insert into activity (activity_id, attraction_id, name) values (%s, %s, %s)", )
         rows2=cursor.fetchall()
         session['attraction_id'] = rows[0][0]
     cursor.close()
-    cursor.close()
     return render_template('attraction.html', attraction=attraction,
-                           columns=column_names, rows=rows, form=form)
+                           columns=column_names, rows=rows, form=form, activitynames=activitynames)
 
 if __name__ == '__main__':
     dbname = 'team5_schema'
