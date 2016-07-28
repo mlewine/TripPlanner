721efb16bc6ae83422cbf5c7507dac8a5aa43525
Sidd Meka <Sidd@Siddharthas-MacBook-Air.local>
2016-07-28 15:30:34 +0200

    stuff

--- TripWebApp.py
+++ TripWebApp.py
@@ -35,14 +35,14 @@ class RegistrationForm(Form):
 
 #Create form to write a review
 class ReviewForm(Form):
-    attraction_review=StringField('Attraction Name', validators=[Required()])
+    attraction_review=SelectField(u'Attraction Name', choices=[('Metz Cathedral','Metz Cathedral'), ('Centre Pompidou-Metz','Centre Pompidou-Metz'), ('Museums of Metz','Museums of Metz'),('Eiffel Tower','Eiffel Tower'),('Louvre','Louvre'),('Notre Dame','Notre Dame'),('58 Tour Eiffel','58 Tour Eiffel'),('Brunch boat cruise','Brunch boat cruise'),('Colosseum','Colosseum'),('Pantheon','Pantheon'),('Trevi Fountain','Trevi Fountain')])
     title=StringField('Title of Review',validators=[Required()])
     review=StringField('Review', validators=[Required()])
     date=StringField('Date (yyyy-mm-dd)',validators=[Required()])
     submit = SubmitField('Submit')
 
 class CreateTripForm(Form):
-    trip_city=SelectField(u'Programming Language', choices=[('Metz','Metz'), ('Paris','Paris'), ('Rome','Rome')])
+    trip_city=SelectField(u'City', choices=[('Metz','Metz'), ('Paris','Paris'), ('Rome','Rome')])
     trip_start_date = StringField('Date (yyyy-mm-dd)',validators=[Required()]) 
     submit = SubmitField('Submit')
 
